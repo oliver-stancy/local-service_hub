8caff3da1c4c2bf3253b6e315a6601606a9de072
unknown <oliverstancylugala@gmail.com>
2026-03-09 11:32:43 +0300

    first commit

--- local_servicehub/settings.py
+++ local_servicehub/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p2ag)8x=(vsd5)&oem7h%lrf^bfone81fjpbo$a9m5^6bu^)6s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
